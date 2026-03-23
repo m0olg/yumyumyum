7e9db19c2caa68e77918d033a7cca7c20a56218b
abc <abc@abcui-MacBookPro.local>
2026-03-23 16:04:32 +0900

    Text를 ios로 수정

---
+++ 닭발먹고싶다 오물결/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  닭발먹고싶다 오물결
+//
+//  Created by abc on 3/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 닭발먹고싶다 오물결/_________________________App.swift
@@ -0,0 +1,17 @@
+//
+//  _________________________App.swift
+//  닭발먹고싶다 오물결
+//
+//  Created by abc on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct _________________________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
